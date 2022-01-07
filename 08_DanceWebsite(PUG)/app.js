930199d436370218e898c74c50497d76b3b6c56a
Udit8158 <kunduudit8158@gmail.com>
2022-01-07 21:19:38 +0530

    Fully complete the dance website

--- 08_DanceWebsite(PUG)/app.js
+++ 08_DanceWebsite(PUG)/app.js
@@ -1,41 +1,63 @@
 //DEFINING REQUIRED VRIABLES
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const hostname = '127.0.0.1';
-// Include mongoose 
-const mongoose = require('mongoose');
-const bodyparser = require("body-parser"); //Include body-parser for collect data from form as an input
+const hostname = "127.0.0.1";
+const bodyParser = require("body-parser"); //Include body-parser for collect data from form as an input
 
+//Use bodyParser
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 //Start mongoose with mongoDb
-//1. Setting the schema
-const contactSchema = new mongoose.Schema({
+
+//connect the mongo db
+const mongoose = require("mongoose");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/danceWebsiteForm");
+}
+//Setting the schema
+const contactFormSchema = new mongoose.Schema({
   name: String,
   phone: String,
   address: String,
   thoughts: String,
 });
 
-//2. Creating a model from the schema
-const Contact = mongoose.model('Kitten', contactSchema);
+//Making a model from the schema
+const form = mongoose.model("contactForm", contactFormSchema);
 
-// EXPRESS STUFF 
-app.use('/static',express.static('static'));//Using static folder
+// EXPRESS STUFF
+app.use("/static", express.static("static")); //Using static folder
 
 //PUG RELATED STUFF
-app.set('view engine', 'pug'); //setting pug as view engine
-
+app.set("view engine", "pug"); //setting pug as view engine
 
 //SETTING ENDPOINTS
-app.get('/', function (req, res) {
-    res.render('home.pug'); //Menans rendering index.pug file from views folder
-  })
-app.get('/contact', function (req, res) {
-    res.render('contact.pug'); //Menans rendering index.pug file from views folder
-  })
+app.get("/", function (req, res) {
+  res.render("home.pug"); //Menans rendering index.pug file from views folder
+});
+app.get("/contact", function (req, res) {
+  res.render("contact.pug"); //Menans rendering index.pug file from views folder
+});
+app.post("/contact", (req, res) => {
+  var myData = new form(req.body);
+  myData
+    .save()
+    .then(() => {
+      res.send("This form has submitted successfully.");
+    })
+    .catch(() => {
+      res.status(400).send("This form has not submitted. ");
+    });
+});
 
-
-//STARTING SERVER   
-app.listen(port,()=>{
-    console.log(`Server is running on http://${hostname}:${port}`);
-})
+//STARTING SERVER
+app.listen(port, () => {
+  console.log(`Server is running on http://${hostname}:${port}`);
+});
